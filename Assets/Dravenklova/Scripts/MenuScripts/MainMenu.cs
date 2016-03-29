a2f79455512a9621603f5e6723c0ca7cae3ba20f
LesVox <joskarb@gmail.com>
2016-03-29 17:37:21 +0200

    Added menu, started working on the MVP Healtbar, and told your mother she a purdi lady

---
+++ Assets/Dravenklova/Scripts/MenuScripts/MainMenu.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour
+{
+    private int m_ButtonWidth = 200;
+    private int m_ButtonHeight = 50;
+    private int m_ButtonSpacing = 50;
+
+    void OnGUI()
+    {
+        if (GUI.Button(new Rect(Screen.width / 2 - m_ButtonWidth / 2, Screen.height / 2 - m_ButtonHeight / 2 - m_ButtonSpacing, m_ButtonWidth, m_ButtonHeight), "Click Bait"))
+        {
+            Application.LoadLevel("MainScene");
+        }
+
+        if (GUI.Button(new Rect(Screen.width / 2 - m_ButtonWidth / 2, Screen.height / 2 + m_ButtonHeight / 2 + m_ButtonSpacing, m_ButtonWidth, m_ButtonHeight), "Quit Bait"))
+        {
+            Application.Quit();
+
+        }
+    }
+
+
+}
