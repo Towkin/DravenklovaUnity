94e540effbb5c51fc3a07b1b9c30757577e66e6d
LesVox <joskarb@gmail.com>
2016-03-29 18:15:59 +0200

    Completed MVP Healthbar, all that you need to do is to add a UI prefab in the scene

---
+++ Assets/Dravenklova/Scripts/PawnScripts/PlayerScripts/HealthBar.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class HealthBarScript : MonoBehaviour
+{
+    private Player m_Player;
+
+    void FixedUpdate ()
+    {
+        if(m_Player == null)
+        {
+            m_Player = FindObjectOfType<Player>();
+            if(m_Player == null)
+            {
+                return;
+            }
+        }
+        Image m_HealthBar = GetComponent<Image>();
+        m_HealthBar.fillAmount = m_Player.Health;
+        
+    }
+}
