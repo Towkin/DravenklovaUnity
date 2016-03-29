6a3242901909f6716644cd61960d282f39d4053e
unknown <stroem@3se-spel-26.utb.fuv.se>
2016-03-29 17:28:09 +0200

    FMOD Integration added, ambience sounds added, player character "involuntary jump" fixed.

---
+++ Assets/Dravenklova/Scripts/PawnScripts/PlayerScripts/CreateEmitters.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CreateEmitters : MonoBehaviour {
+
+    [SerializeField]
+    [Range(0, 100f)]
+    private float m_RangeMin = 10f;
+    [SerializeField]
+    [Range(0, 100f)]
+    private float m_RangeMax = 50f;
+
+    [SerializeField]
+    [Range(0, 60f)]
+    private float m_TimerMax = 4f;
+    [SerializeField]
+    [Range(0, 60f)]
+    private float m_TimerMin = 2f;
+    private float TimerRandom
+    {
+        get
+        {
+            float ReturnVal = Random.Range(m_TimerMin, m_TimerMax);
+
+            //Debug.Log(ReturnVal.ToString());
+
+            return ReturnVal;
+        }
+    }
+
+    [SerializeField]
+    private GameObject[] m_TemplateEmitter;
+
+    private float m_Timer = 0f;
+    private float Timer
+    {
+        get { return m_Timer; }
+        set
+        {
+            m_Timer = value;
+
+            if(m_Timer <= 0f)
+            {
+                CreateEmitter();
+                m_Timer = TimerRandom;
+            }
+        }
+    }
+
+    void Start ()
+    {
+        Timer = TimerRandom;
+    }
+
+	void Update ()
+    {
+        Timer -= Time.deltaTime;
+	}
+
+
+    void CreateEmitter()
+    {
+        if (m_TemplateEmitter.Length <= 0)
+        {
+            Debug.LogError("No objects in Template Emitter array!");
+            return;
+        }
+
+        GameObject Instance = Instantiate(m_TemplateEmitter[Random.Range(0, m_TemplateEmitter.Length)]);
+
+        Instance.transform.position = transform.position + Random.onUnitSphere * Random.Range(m_RangeMin, m_RangeMax);
+    }
+}
